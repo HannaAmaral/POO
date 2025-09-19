25f510408522b8d95c27ee734c7ad88917cff69a
Hanna Amaral <hannaap0001@gmail.com>
2025-09-19 20:45:52 -0300

    implementação de Herança

---
+++ 09_Heranca/Pessoa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Heranca
+{
+    internal abstract class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome;
+        public string Telefone;
+
+        public abstract string getDocumento();
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Lista de Pessoas \n\nNome: {this.Nome} Documento: {this.getDocumento()} Telefone: {this.Telefone}");
+        }
+        
+        public override string ToString()
+        {
+            return $"<ToString> Nome: {this.Nome} Documento: {getDocumento()} Telefone: {this.Telefone}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            //Cast=> Conversão de objeto
+            //((Pessoa) obj)=> Converte o obj para o tipo Pessoa
+            return this.Id == ((Pessoa)obj).Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+    }
+}
---
+++ 09_Heranca/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pessoa obj = new Pessoa();   não pode instanciar uma classe abstrata
+            Pessoa[] pessoas = new Pessoa[2];
+
+            PessoaFisica pessoaFisica= new PessoaFisica();
+            pessoas[0] = pessoaFisica;
+            pessoaFisica.Nome = "Geraldo";
+            pessoaFisica.CPF = "999.999.999-99";
+            pessoaFisica.Telefone = "(99) 99999-9999";  
+            pessoaFisica.Imprimir();
+
+            PessoaJuridica pessoaJuridica = new PessoaJuridica();
+            pessoas[1] = pessoaJuridica;
+            pessoaJuridica.Nome = "Senac Marília";
+            pessoaJuridica.CNPJ = "99.999.999-99 / 9999-99";
+            pessoaJuridica.Telefone = "(99) 99999-9999";
+            pessoaJuridica.Imprimir();
+
+            Console.WriteLine(pessoaFisica);
+            Console.WriteLine(pessoaJuridica);
+
+            if (pessoaFisica.Equals(pessoaJuridica))
+                Console.WriteLine("É a mesma pessoa.");
+            else
+                Console.WriteLine("Não é a mesma pessoa.");
+
+            foreach (Pessoa pessoa in pessoas)
+            {
+                Console.WriteLine(pessoa);
+            }
+        }
+    }
+}
